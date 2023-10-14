268dad1c8b1235c35440234bf6333ae4b603376a
resting <roy.wong.80@gmail.com>
2023-10-14 12:55:47 +0800

    Test shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import {Button} from "@/components/ui/button"
 import './App.css'
 
 function App() {
 
   return (
-    <h1>hello</h1>
+    <>
+      <h1>hello</h1>
+      <Button>Click me</Button>
+    </>
   )
 }
 
